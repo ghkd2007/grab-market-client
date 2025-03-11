f767cef92a04628b87b8f58192564907879035b0
junhi <ghkd2007@gmail.com>
2025-03-11 21:45:59 +0900

    오류 수정(변수명)

--- src/products/index.js
+++ src/products/index.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 function ProductPage() {
 	const { id } = useParams();
-	console.log(params);
+	console.log(id);
 	return <h1>상품 상세 {id} 상품</h1>;
 }
 
