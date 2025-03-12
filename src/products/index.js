42c77de9159e7ed430cb2a460f6e86762f431a87
junhi <ghkd2007@gmail.com>
2025-03-12 22:22:02 +0900

    상세 정보 페이지 CSS 만들기

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
 	const { id } = useParams();
@@ -18,8 +19,28 @@ function ProductPage() {
 			});
 		console.log(id);
 	}, []);
-	console.log(product);
-	return <h1>상품 상세 {id} 상품</h1>;
+
+	if (product === null) {
+		return <h1>상품 정보를 받고 있습니다...</h1>;
+	}
+
+	return (
+		<div>
+			<div id="image-box">
+				<img src={"/" + product.imageUrl} />
+			</div>
+			<div id="profile-box">
+				<img src="/images/icons/avatar.png" />
+				<span>{product.seller}</span>
+			</div>
+			<div id="contents-box">
+				<div id="name">{product.name}</div>
+				<div id="price">{product.price}원</div>
+				<div id="createdAt">2025년 3월 12일</div>
+				<div id="description">{product.description}</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductPage;
