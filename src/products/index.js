443912e1ed0e6e9905838de4cbbae3c49f6b96d2
junhi <ghkd2007@gmail.com>
2025-03-12 21:13:06 +0900

    useEffect, useState 적용

--- src/products/index.js
+++ src/products/index.js
@@ -1,8 +1,24 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
 	const { id } = useParams();
-	console.log(id);
+	const [product, setProduct] = useState(null);
+	useEffect(function () {
+		axios
+			.get(
+				`https://7ef32337-8018-457a-916c-6fb2e1c0ee54.mock.pstmn.io/products/${id}`
+			)
+			.then(function (result) {
+				setProduct(result.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+		console.log(id);
+	}, []);
+	console.log(product);
 	return <h1>상품 상세 {id} 상품</h1>;
 }
 
