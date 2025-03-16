28f110fdd3019ef03ee7d9efaed89f0611ff5474
junhi <ghkd2007@gmail.com>
2025-03-16 22:09:06 +0900

    ANTD 라이브러리 사용, 업로드 버튼 추가, useNavigate 사용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { Button } from "antd";
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main/index.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+	useNavigate,
+} from "react-router-dom";
 import UploadPage from "./upload/index.js";
 import ProductPage from "./products/index.js";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+	const history = useNavigate();
 	return (
 		<div>
 			<div id="header">
 				<div id="header-area">
-					<img src="/images/icons/logo.png" alt="" />
+					<Link to="/">
+						<img src="/images/icons/logo.png" alt="" />
+					</Link>
+					<Button
+						size="large"
+						onClick={function () {
+							history("/upload");
+						}}
+						icon={<DownloadOutlined />}
+					>
+						상품 업로드
+					</Button>
 				</div>
 			</div>
 			<div id="body">
